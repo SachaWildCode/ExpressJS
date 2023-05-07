7d4382e447918747db30425a607655847d0bad5d
SachaWildCode <sacha.loumachi@gmail.com>
2023-05-07 14:44:51 +0200

    add put

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 
+const { validateMovie } = require("./validators.js")
+
 require("dotenv").config();
 
 const app = express();
@@ -15,18 +17,22 @@ const welcome = (req, res) => {
 app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
-const userHandlers = require("./userHandlers")
+const userHandlers = require("./userHandlers");
+
 
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
 app.post("/api/users", userHandlers.postUser);
 
+app.put("/api/users/:id", userHandlers.updateUser);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/movies",validateMovie,movieHandlers.postMovie);
+
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.listen(port, (err) => {
   if (err) {
