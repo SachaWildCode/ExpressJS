c00f07645fb1823a933b2f8fab18b40bef10c2be
SachaWildCode <sacha.loumachi@gmail.com>
2023-05-09 16:30:44 +0200

    add post users

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
 const port = process.env.APP_PORT ?? 5005;
 
 const welcome = (req, res) => {
@@ -18,6 +19,8 @@ app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUsersById);
+app.post("/api/movies", movieHandlers.postMovie);
+app.post("/api/users", userHandlers.postUsers);
 
 app.listen(port, (err) => {
   if (err) {
